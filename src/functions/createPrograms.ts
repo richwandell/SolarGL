9879b87e7d13be17ffd0e47fe183c43dc8d4facb
Rich Wandell <richwandell@gmail.com>
2021-01-24 15:06:02 -0500

    working more on lighting

--- src/functions/createPrograms.ts
+++ src/functions/createPrograms.ts
@@ -1,40 +1,7 @@
-import {ColoredProgram, TexturedProgram} from "../types";
-import {
-    coloredFragment,
-    coloredTexturedShadedFragment,
-    coloredTexturedShadedVertex,
-    simpleVertexColored
-} from "../shaders";
+import {TexturedProgram} from "../types";
+import {coloredTexturedShadedFragment, coloredTexturedShadedVertex} from "../shaders";
 import SolarMesh from "../SolarMesh";
 
-function createColored(gl: WebGL2RenderingContext): ColoredProgram {
-    const vertexShader = gl.createShader(gl.VERTEX_SHADER) as WebGLShader
-    gl.shaderSource(vertexShader, simpleVertexColored)
-    gl.compileShader(vertexShader)
-
-    const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER) as WebGLShader
-    gl.shaderSource(fragmentShader, coloredFragment)
-    gl.compileShader(fragmentShader)
-
-    const shaderProgram = gl.createProgram() as WebGLProgram
-    gl.attachShader(shaderProgram, vertexShader)
-    gl.attachShader(shaderProgram, fragmentShader)
-    gl.linkProgram(shaderProgram)
-
-    return {
-        program: shaderProgram as WebGLProgram,
-        attribLocations: {
-            vertexPosition: gl.getAttribLocation(shaderProgram, 'aVertexPosition'),
-            vertexColor: gl.getAttribLocation(shaderProgram, 'aVertexColor'),
-            vertexNormal: gl.getAttribLocation(shaderProgram, 'aVertexNormal')
-        },
-        uniformLocations: {
-            projectionMatrix: gl.getUniformLocation(shaderProgram, 'uProjectionMatrix') as WebGLUniformLocation,
-            modelViewMatrix: gl.getUniformLocation(shaderProgram, 'uModelViewMatrix') as WebGLUniformLocation,
-            normalMatrix: gl.getUniformLocation(shaderProgram, 'uNormalMatrix') as WebGLUniformLocation,
-        },
-    }
-}
 
 function createTextured(gl: WebGL2RenderingContext): TexturedProgram {
     const vertexShader = gl.createShader(gl.VERTEX_SHADER) as WebGLShader
